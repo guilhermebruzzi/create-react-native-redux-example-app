1d07bc37a4b264d5caa18e5498b18545e516ca22
Guilherme Bruzzi <guibruzzi@gmail.com>
2017-03-19 19:42:32 -0300

    Add REDUX_DEVTOOLS_EXTENSION_COMPOSE as a redux middleware

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux'
+import { createStore, applyMiddleware, combineReducers, compose } from 'redux'
 import thunk from 'redux-thunk'
 import counters from './modules/counters'
 
@@ -9,5 +9,20 @@ const reducer = combineReducers({
 const middleware = applyMiddleware(thunk)
 
 export default function createAppStore(initialValue = {}) {
-  return createStore(reducer, initialValue, middleware)
+  let store
+
+  if (process.env.NODE_ENV === 'development') {
+    // Development mode with Redux DevTools support enabled.
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+      // Prevents Redux DevTools from re-dispatching all previous actions.
+      shouldHotReload: false,
+    }) : compose
+    // Create the redux store.
+    store = createStore(reducer, initialValue, composeEnhancers(middleware))
+  } else {
+    // Production mode.
+    store = createStore(reducer, initialValue, middleware)
+  }
+
+  return store
 }
